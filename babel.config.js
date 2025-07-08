56d812ecfb21e954a476eccd71537ebddad928c4
Soberanalysts <snplab@SNPLabui-MacStudio-2.local>
2025-07-08 10:10:02 +0900

    해시값 불일치 에러

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     '@babel/plugin-transform-nullish-coalescing-operator',
     '@babel/plugin-transform-optional-chaining',
