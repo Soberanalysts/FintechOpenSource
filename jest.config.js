b6cee9f1405e25c968c5085590e95b4686f06c16
Soberanalysts <snplab@SNPLabui-MacStudio-2.local>
2025-07-09 14:06:55 +0900

    tsconfig.jest.json 추가

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,23 @@
+// module.exports = {
+//   preset: 'react-native',
+//   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // 💡 테스트 환경 확장 (예: matchers)
+//   transformIgnorePatterns: [
+//     'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|react-redux)',
+//   ],
+//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+//   testMatch: ['**/__tests__/**/*.test.(js|ts|tsx)'],
+// };
 module.exports = {
   preset: 'react-native',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // 💡 테스트 환경 확장 (예: matchers)
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)',
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|react-redux)',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testMatch: ['**/__tests__/**/*.test.(js|ts|tsx)'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json'
+    }
+  }
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,10 @@
 import '@testing-library/jest-native/extend-expect'; // 🔥 toHaveTextContent 등 사용 가능
 import 'react-native-gesture-handler/jestSetup';
 
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
+
+
 // 필요시 모킹할 것들 (예: react-navigation)
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
